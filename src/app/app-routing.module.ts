56f0bf5973fdc3ae50b4b1fc1e6251159d3d79e2
likaipeng <lkpTomorrow@gmail.com>
2020-05-10 21:44:12 +0800

    feat(): 配置路由及重定向

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from "./components/product/product.component";
+import { HomeComponent } from "./components/home/home.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { NewsComponent } from "./components/news/news.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: "**", redirectTo: 'news' },
+  { path: "home", component: HomeComponent },
+  { path: "news", component: NewsComponent },
+  { path: "product", component: ProductComponent },
+  { path: "**", redirectTo: 'news' }, // 默认就调到home组件;从上往下匹配，有匹配到的就结束。只能放在最后。
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
